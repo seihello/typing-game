f1a470d12667ca76de001ed03f8da5da2d26e0e5
Seisuke Yamada <momoiropuchoman@gmail.com>
2026-01-09 04:22:12 +0900

    improvement: prevent from coping target sentence

--- components/target-sentence.tsx
+++ components/target-sentence.tsx
@@ -3,5 +3,15 @@ type Props = {
 }
 
 export default function TargetSentence({ value }: Props) {
-  return <div className="text-2xl font-bold">{value}</div>
+  return (
+    <div
+      className="text-2xl font-bold"
+      style={{
+        WebkitUserSelect: "none",
+        userSelect: "none",
+      }}
+    >
+      {value}
+    </div>
+  )
 }
