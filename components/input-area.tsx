be33d2114b4020df92ba85597b5ddac07435fcc9
Seisuke Yamada <momoiropuchoman@gmail.com>
2026-01-09 01:00:25 +0900

    feat: set color to specific character

--- components/input-area.tsx
+++ components/input-area.tsx
@@ -1,8 +1,34 @@
 "use client"
 
+import { Mark, mergeAttributes } from "@tiptap/core"
 import { EditorContent, useEditor } from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
 import { Dispatch, SetStateAction } from "react"
+
+const SpanColor = Mark.create({
+  name: "spanColor",
+  priority: 1000,
+  // 全ての span タグをこの Mark として認識させる
+  parseHTML() {
+    return [{ tag: "span" }]
+  },
+  // attributes として style を維持するように設定
+  addAttributes() {
+    return {
+      style: {
+        default: null,
+        parseHTML: (element) => element.getAttribute("style"),
+        renderHTML: (attributes) => {
+          if (!attributes.style) return {}
+          return { style: attributes.style }
+        },
+      },
+    }
+  },
+  renderHTML({ HTMLAttributes }) {
+    return ["span", mergeAttributes(HTMLAttributes), 0]
+  },
+})
 
 type Props = {
   value: string
@@ -11,19 +37,30 @@ type Props = {
 
 export default function InputArea({ value, setValue }: Props) {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [StarterKit, SpanColor],
     content: "",
     immediatelyRender: false,
     onUpdate: ({ editor }) => {
       setValue(editor.getText())
+
+      const originalText = editor.getText()
+
+      const coloredText = originalText.replace(
+        /あ/g,
+        '<span style="color: red;">あ</span>',
+      )
+
+      if (originalText !== coloredText) {
+        const { from, to } = editor.state.selection
+        editor.commands.setContent(coloredText, { emitUpdate: false })
+        editor.commands.setTextSelection({ from, to })
+      }
     },
   })
 
   if (!editor) {
     return null
   }
-
-  console.log("input", value)
 
   return (
     <EditorContent
