feac35cb2d98da0c59143da0aa8ae36a8a8e1be4
Seisuke Yamada <momoiropuchoman@gmail.com>
2026-01-09 03:35:14 +0900

    refactor: move tiptap extension

--- components/input-area.tsx
+++ components/input-area.tsx
@@ -1,34 +1,9 @@
 "use client"
 
-import { Mark, mergeAttributes } from "@tiptap/core"
+import { SpanColor } from "@/tiptap/extensions"
 import { EditorContent, useEditor } from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
 import { Dispatch, SetStateAction } from "react"
-
-const SpanColor = Mark.create({
-  name: "spanColor",
-  priority: 1000,
-  // 全ての span タグをこの Mark として認識させる
-  parseHTML() {
-    return [{ tag: "span" }]
-  },
-  // attributes として style を維持するように設定
-  addAttributes() {
-    return {
-      style: {
-        default: null,
-        parseHTML: (element) => element.getAttribute("style"),
-        renderHTML: (attributes) => {
-          if (!attributes.style) return {}
-          return { style: attributes.style }
-        },
-      },
-    }
-  },
-  renderHTML({ HTMLAttributes }) {
-    return ["span", mergeAttributes(HTMLAttributes), 0]
-  },
-})
 
 type Props = {
   value: string
---
+++ tiptap/extensions.ts
@@ -0,0 +1,26 @@
+import { Mark, mergeAttributes } from "@tiptap/core"
+
+export const SpanColor = Mark.create({
+  name: "spanColor",
+  priority: 1000,
+  // 全ての span タグをこの Mark として認識させる
+  parseHTML() {
+    return [{ tag: "span" }]
+  },
+  // attributes として style を維持するように設定
+  addAttributes() {
+    return {
+      style: {
+        default: null,
+        parseHTML: (element) => element.getAttribute("style"),
+        renderHTML: (attributes) => {
+          if (!attributes.style) return {}
+          return { style: attributes.style }
+        },
+      },
+    }
+  },
+  renderHTML({ HTMLAttributes }) {
+    return ["span", mergeAttributes(HTMLAttributes), 0]
+  },
+})
