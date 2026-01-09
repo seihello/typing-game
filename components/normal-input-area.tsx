61d2205abfa4febaa17a8f7a0d38eb913233759a
Seisuke Yamada <momoiropuchoman@gmail.com>
2026-01-10 02:23:24 +0900

    fix: fix issue where target text color changes before converting is complete

--- components/normal-input-area.tsx
+++ components/normal-input-area.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Dispatch, SetStateAction } from "react"
+import { Dispatch, SetStateAction, useRef, useState } from "react"
 
 type Props = {
   input: string
@@ -8,11 +8,26 @@ type Props = {
 }
 
 export default function NormalInputArea({ input, setInput }: Props) {
+  const [value, setValue] = useState(input)
+  const isComposing = useRef(false)
+
   return (
     <textarea
       className="h-64 w-full bg-white p-2 text-2xl font-bold text-black outline-none"
-      value={input}
-      onChange={(e) => setInput(e.target.value)}
+      value={value}
+      onCompositionStart={() => {
+        isComposing.current = true
+      }}
+      onCompositionEnd={() => {
+        isComposing.current = false
+        setInput(value)
+      }}
+      onChange={(e) => {
+        setValue(e.target.value)
+        if (!isComposing.current) {
+          setInput(e.target.value)
+        }
+      }}
     />
   )
 }
