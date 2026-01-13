6e06b53577ddeea8d1e59533cf7ae40b07622f88
Seisuke Yamada <momoiropuchoman@gmail.com>
2026-01-13 22:12:01 +0900

    style: align result title right

--- components/result-view.tsx
+++ components/result-view.tsx
@@ -9,17 +9,17 @@ export default function ResultView({ result }: Props) {
     <div className="flex h-64 w-full flex-col items-center justify-center gap-y-4">
       <table className="[&_td]:px-4 [&_td]:py-2">
         <tr>
-          <td className="font-semibold">入力した文字数</td>
+          <td className="text-right font-semibold">入力した文字数</td>
           <td className="text-2xl font-bold">{result.wordCount}字</td>
         </tr>
         <tr>
-          <td className="font-semibold">かかった時間</td>
+          <td className="text-right font-semibold">かかった時間</td>
           <td className="text-2xl font-bold">
             {(result.elapsedTime / 1000).toFixed(1)}秒
           </td>
         </tr>
         <tr>
-          <td className="font-semibold">平均タイプ数</td>
+          <td className="text-right font-semibold">平均タイプ数</td>
           <td className="text-2xl font-bold">
             {result.elapsedTime > 0
               ? (result.wordCount / (result.elapsedTime / 1000)).toFixed(1)
