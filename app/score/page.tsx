80a1972663e677c8c8cb6e61f71e9b28d8221676
Seisuke Yamada <momoiropuchoman@gmail.com>
2026-02-03 00:46:17 +0900

    improvement: update how to show score

--- app/score/page.tsx
+++ app/score/page.tsx
@@ -22,33 +22,37 @@ export default function ScorePage() {
 
   if (!result) return
 
+  const { score, grade } = calcScore(result)
+
   return (
     <div className="mx-auto flex min-h-screen max-w-3xl flex-col items-center px-4 py-8 text-white">
-      <div className="flex w-full flex-col items-center justify-center gap-y-8">
-        <table className="[&_td]:px-4 [&_td]:py-2">
-          <tr>
-            <td className="text-right font-semibold">入力した文字数</td>
-            <td className="text-2xl font-bold">{result.wordCount}字</td>
-          </tr>
-          <tr>
-            <td className="text-right font-semibold">かかった時間</td>
-            <td className="text-2xl font-bold">
-              {(result.elapsedTime / 1000).toFixed(1)}秒
-            </td>
-          </tr>
-          <tr>
-            <td className="text-right font-semibold">平均タイプ数</td>
-            <td className="text-2xl font-bold">
-              {result.elapsedTime > 0
-                ? (result.wordCount / (result.elapsedTime / 1000)).toFixed(1)
-                : ""}
-              文字 / 秒
-            </td>
-          </tr>
-        </table>
-        <div className="flex flex-col items-center gap-x-2">
-          <h2 className="font-semibold">総合スコア</h2>
-          <p className="text-6xl font-bold">{calcScore(result)}</p>
+      <div className="flex w-full flex-col items-center justify-center gap-y-12">
+        <div className="space-y-8">
+          <table className="[&_td]:px-4 [&_td]:py-2">
+            <tr>
+              <td className="text-right font-semibold">入力した文字数</td>
+              <td className="text-2xl font-bold">{result.wordCount}字</td>
+            </tr>
+            <tr>
+              <td className="text-right font-semibold">かかった時間</td>
+              <td className="text-2xl font-bold">
+                {(result.elapsedTime / 1000).toFixed(1)}秒
+              </td>
+            </tr>
+            <tr>
+              <td className="text-right font-semibold">平均タイプ数</td>
+              <td className="text-2xl font-bold">
+                {result.elapsedTime > 0
+                  ? (result.wordCount / (result.elapsedTime / 1000)).toFixed(1)
+                  : ""}
+                文字 / 秒
+              </td>
+            </tr>
+          </table>
+          <div className="flex flex-col items-center gap-y-2">
+            <h2 className="font-semibold">総合スコア</h2>
+            <p className="text-7xl font-bold tracking-wide">{score}</p>
+          </div>
         </div>
         <Button
           variant="secondary"
@@ -71,24 +75,28 @@ export default function ScorePage() {
   )
 }
 
-function calcScore(result: Result) {
-  if (result.elapsedTime <= 0) {
-    return "判定不能"
-  }
+function calcScore(result: Result): { score: number; grade: string } {
+  const timeInSeconds = result.elapsedTime / 1000
+  // const wordPerSecond = result.wordCount / timeInSeconds
 
-  const wordPerSecond = result.wordCount / (result.elapsedTime / 1000)
+  const kps = result.wordCount / timeInSeconds
 
-  if (wordPerSecond > 2) {
-    return "S"
-  } else if (wordPerSecond > 1.5) {
-    return "A"
-  } else if (wordPerSecond > 1) {
-    return "B"
-  } else if (wordPerSecond > 0.6) {
-    return "C"
-  } else if (wordPerSecond > 0.3) {
-    return "D"
+  const score = Math.floor(kps * result.wordCount * 9)
+
+  let grade = ""
+  if (score > 10000) {
+    grade = "S"
+  } else if (score > 8000) {
+    grade = "A"
+  } else if (score > 6000) {
+    grade = "B"
+  } else if (score > 4000) {
+    grade = "C"
+  } else if (score > 2000) {
+    grade = "D"
   } else {
-    return "E"
+    grade = "E"
   }
+
+  return { score, grade }
 }
